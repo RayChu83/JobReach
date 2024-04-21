419b418c655698ca077d4ea9799e6d9f46319b54
Ray <rayc12079@gmail.com>
2024-04-21 02:24:02 -0400

    Created preview jobs component and the jobs page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/Hero'
+import PreviewJobs from '@/components/PreviewJobs'
 import React from 'react'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <PreviewJobs />
     </main>
   )
 }
---
+++ src/components/PreviewJobs.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "next-view-transitions";
+import { Button } from "@/components/ui/button";
+
+export default function PreviewJobs() {
+  return (
+    <div className="max-w-[1280px] m-auto p-4 ">
+      <section className="grid sm:grid-cols-2 grid-cols-1 gap-4 mb-4 ">
+        <article className="bg-gray-100 p-4 rounded-sm drop-shadow-sm">
+          <h1 className="text-xl font-medium">Front-End Developer</h1>
+          <small className="text-gray-500">Google</small>
+          <p className="line-clamp-2 overflow-hidden">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel
+            voluptate itaque dicta!
+          </p>
+        </article>
+        <article className="bg-gray-100 p-4 rounded-sm drop-shadow-sm">
+          <h1 className="text-xl font-medium">Back-End Developer</h1>
+          <small className="text-gray-500">Microsoft</small>
+          <p className="line-clamp-2 overflow-hidden">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+            ea officia libero velit vitae.
+          </p>
+        </article>
+        <article className="bg-gray-100 p-4 rounded-sm drop-shadow-sm">
+          <h1 className="text-xl font-medium">Full Stack Developer</h1>
+          <small className="text-gray-500">Google</small>
+          <p className="line-clamp-2 overflow-hidden">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+            dolor beatae itaque asperiores eum! Temporibus earum dolore totam
+            maxime nulla, nostrum ipsa!
+          </p>
+        </article>
+        <article className="bg-gray-100 p-4 rounded-sm drop-shadow-sm">
+          <h1 className="text-xl font-medium">UI UX Designer</h1>
+          <small className="text-gray-500">Amazon</small>
+          <p className="line-clamp-2 overflow-hidden">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni odio
+            dignissimos alias blanditiis animi?
+          </p>
+        </article>
+      </section>
+      <div className="flex justify-center">
+        <Button className="m-auto" variant="cta" asChild>
+          <Link href="/jobs">See All Jobs</Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
