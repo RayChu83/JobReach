d3ea2684c81bbd9cc706cd529e6b1b941666b6d6
Ray <rayc12079@gmail.com>
2024-04-21 20:37:17 -0400

    Error handling testing 3

--- src/app/companies/page.jsx
+++ src/app/companies/page.jsx
@@ -4,40 +4,40 @@ import React from "react";
 const getCompanies = async () => {
   "use server";
   try {
-    console.log(`Fetching URL : ${process.env.NEXT_PUBLIC_URL}api/companies`)
-    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}api/companies`, {method : "get"});
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}api/companies`, {
+      method: "get",
+    });
     const { companies } = await res.json();
     return companies;
   } catch (error) {
-    // throw new Error("Failed to load companies, please try again!");
-    console.log("Failed to load companies, please try again!")
+    console.log("Failed to load companies, please try again!");
   }
 };
 
 export default async function Companies() {
-  const companies = await getCompanies()
-  console.log(companies)
+  const companies = await getCompanies();
   return (
     <main className="max-w-[1280px] p-4 m-auto grid sm:grid-cols-2 md:grid-cols-3 grid-cols-1p-4 gap-4">
-      {companies && companies.map((company) => (
-        <article
-          key={company._id}
-          className="bg-[#F5F5F5] p-4 rounded-sm drop-shadow-sm"
-        >
-          <Link
-            href={`/companies/${company._id}`}
-            className="text-xl font-medium hover:underline"
+      {companies &&
+        companies.map((company) => (
+          <article
+            key={company._id}
+            className="bg-[#F5F5F5] p-4 rounded-sm drop-shadow-sm"
           >
-            {company.name}
-          </Link>
-          <br />
-          <small className="text-gray-500">
-            {company.listings.length} job{company.listings.length !== 1 && "s"}{" "}
-            open
-          </small>
-          <p className="line-clamp-4">{company.description}</p>
-        </article>
-      ))}
+            <Link
+              href={`/companies/${company._id}`}
+              className="text-xl font-medium hover:underline"
+            >
+              {company.name}
+            </Link>
+            <br />
+            <small className="text-gray-500">
+              {company.listings.length} job
+              {company.listings.length !== 1 && "s"} open
+            </small>
+            <p className="line-clamp-4">{company.description}</p>
+          </article>
+        ))}
     </main>
   );
 }
